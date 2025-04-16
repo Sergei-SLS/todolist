083d8d50cf6bdd42ac40dc259ff975e9e84c2bfc
Sergei-SLS <lyamin.sls@gmail.com>
2025-04-16 22:39:21 +0500

    test: add test change todolist filter

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -3,7 +3,7 @@ import {v1} from "uuid";
 
 
 
-type Actions = DeleteTodolistAction | CreateTodolistAction | ChangeTodolistAC
+type Actions = DeleteTodolistAction | CreateTodolistAction | ChangeTodolistAC | ChangeTodolistFilterAC
 
 const initialState: Todolist[] = []
 
@@ -19,6 +19,9 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
       }
       case 'change_todolistTitle': {
           return state.map(todolist => todolist.id === action.payload.id ? {...todolist, title: action.payload.title} : todolist)
+      }
+      case 'change_todolist_filter' : {
+          return state.map(todolist => todolist.id === action.payload.id ? {...todolist, filter: action.payload.filter} : todolist)
       }
       default:
           return state
@@ -49,3 +52,10 @@ export const changeTodolistTitleAC = (id: string, title: string) => {
 }
 
 export type ChangeTodolistAC = ReturnType<typeof changeTodolistTitleAC>
+
+
+export const changeTodolistFilterAC = (id: string, filter: 'all' | 'active' | 'completed') => {
+    return {type: 'change_todolist_filter', payload: {id, filter}} as const
+}
+
+export type ChangeTodolistFilterAC = ReturnType<typeof changeTodolistFilterAC>
