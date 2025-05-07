0473f9dc48ebd62067d745cac8aee31af8a7a912
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-07 16:40:52 +0500

    refactor: decomposition FilterButtons

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,43 +1,22 @@
-import {FilterValues, Todolist} from "./app/App.tsx";
-import {ChangeEvent} from "react";
+import {Todolist} from "./app/App.tsx";
 import {CreateItemForm} from "./CreateItemForm.tsx";
-import {EditableSpan} from "./EditableSpan.tsx";
-import IconButton from '@mui/material/IconButton'
-import DeleteIcon from '@mui/icons-material/Delete'
-import Button from "@mui/material/Button";
-import Checkbox from '@mui/material/Checkbox';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Box from '@mui/material/Box'
-import {containerSx, getListItemSX} from "./TodolistItem.styles.ts";
-import {useAppSelector} from "./common/hooks/useAppSelector.ts";
-import {selectTasks} from "./model/tasks-selectors.ts";
 import {useAppDispatch} from "./common/hooks/useAppDispatch.ts";
-import {changeTaskStatusAC, changeTaskTitleAC, createTaskAC, deleteTaskAC} from "./model/tasks-reducer.ts";
-import {changeTodolistFilterAC, changeTodolistTitleAC, deleteTodolistAC} from "./model/todolists-reducer.ts";
+import {createTaskAC} from "./model/tasks-reducer.ts";
 import {TodolistTitle} from "./TodolistTitle.tsx";
-import { Tasks } from "./Tasks.tsx";
+import {Tasks} from "./Tasks.tsx";
+import {FilterButtons} from "./FilterButtons.tsx";
 
 type Props = {
     todolist: Todolist
     date?: string
 }
 
-export const TodoListItem = ({
-                                 todolist: {id, title, filter},
-                                 date
-                             }: Props) => {
-
+export const TodoListItem = ({todolist, date}: Props) => {
 
     const dispatch = useAppDispatch()
 
-
     const createTask = (title: string) => {
-        dispatch(createTaskAC({todolistId: id, title}))
-    }
-
-    const changeFilter = (filter: FilterValues) => {
-        dispatch(changeTodolistFilterAC({id, filter}))
+        dispatch(createTaskAC({todolistId: todolist.id, title}))
     }
 
     return (
@@ -45,24 +24,7 @@ export const TodoListItem = ({
             <TodolistTitle todolist={todolist}/>
             <CreateItemForm onCreateItem={createTask}/>
             <Tasks todolist={todolist}/>
-
-            <Box sx={containerSx}>
-                <Button variant={filter === 'all' ? 'outlined' : 'text'}
-                        color={'inherit'}
-                        onClick={() => changeFilter('all')}>
-                    All
-                </Button>
-                <Button variant={filter === 'active' ? 'outlined' : 'text'}
-                        color={'primary'}
-                        onClick={() => changeFilter('active')}>
-                    Active
-                </Button>
-                <Button variant={filter === 'completed' ? 'outlined' : 'text'}
-                        color={'secondary'}
-                        onClick={() => changeFilter('completed')}>
-                    Completed
-                </Button>
-            </Box>
+            <FilterButtons todolist={todolist}/>
             <div style={{marginTop: '10px'}}>{date}</div>
         </div>
     )
